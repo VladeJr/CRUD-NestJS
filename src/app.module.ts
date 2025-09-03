ed992822e98efb7ed9bb62f71ba3f0277d49b18a
Vlade <nekaytsu@gmail.com>
2025-09-02 21:59:21 -0300

    feat(api): Ćonfigurar CORS e validação global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/mypetsync'),
+    PetsModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:5173'], 
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(process.env.PORT || 3000);
+  console.log(`API rodando em http://localhost:${process.env.PORT || 3000}`);
 }
 bootstrap();
